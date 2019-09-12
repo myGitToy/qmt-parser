579b6197baae5806accbf703d9b36d1628bf2bfa
Hui Qiao <G.HUIQIAO@outlook.com>
2019-09-12 20:09:17 +0800

    建立pip自动更新程序

---
+++ pip_update.py
@@ -0,0 +1,11 @@
+import pip
+from subprocess import call
+from pip._internal.utils.misc import get_installed_distributions
+#查看过期的pip库
+pip list --outdated
+pip-review --local --interactive
+for dist in get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
+#————————————————
+#版权声明：本文为CSDN博主「yuzhucu」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+#原文链接：https://blog.csdn.net/yuzhucu/article/details/80287307
