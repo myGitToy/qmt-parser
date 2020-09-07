1a025a9ebe4e19e272bf8adfb8bbc9aeed0bddaf
Hui Qiao <G.HUIQIAO@outlook.com>
2020-09-07 22:06:18 +0800

    调整k_new_high_count默认参数

--- demo/demo_qsp_test.py
+++ demo/demo_qsp_test.py
@@ -1,4 +1,7 @@
 from apt.qsp.k import k
+import pandas as pd
+
 a = k()
-df = a.k_new_high_count(code = '300359' , start = '2019-05-10' , end = '2020-09-06' ,  ktype='D')
+df = a.k_new_high_count(code = '159949' , start = '2019-05-10' , end = '2020-09-07' ,  ktype='60' , MA_HIGH_PERIOD =50)
+df.to_csv('.\\data\\new_high\\test.csv')
 print(df)
