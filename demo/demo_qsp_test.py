133603ea6f458baf4ca5ea36f7dd67638c2f1b60
Hui Qiao <G.HUIQIAO@outlook.com>
2020-09-09 22:02:38 +0800

    测试k线斜率

--- demo/demo_qsp_test.py
+++ demo/demo_qsp_test.py
@@ -1,7 +1,11 @@
 from apt.qsp.k import k
 import pandas as pd
+from analyse import ATR as ATR
 
 a = k()
-df = a.k_new_high_count(code = '159949' , start = '2019-05-10' , end = '2020-09-07' ,  ktype='60' , MA_HIGH_PERIOD =50)
+df = a.k_new_high_count(code = '512880' , start = '2019-05-10' , end = '2020-09-09' ,  ktype='60' , MA_HIGH_PERIOD = 100)
+print(df.getvalue('2020-09-07','close'))
 df.to_csv('.\\data\\new_high\\test.csv')
 print(df)
+atr = ATR()
+atr.ge
