44b7f3b5d33f74b6ba32cda35635931543b18836
Hui Qiao <G.HUIQIAO@outlook.com>
2020-09-11 22:02:51 +0800

    完成新高突破new_high_break

--- apt/qsp/k.py
+++ apt/qsp/k.py
@@ -2,6 +2,7 @@
 from  apt.os.data_load import Data_Load as dl
 from  apt.os.data_update import Data_Update as update
 from datetime import datetime
+import numpy as np
 """
 【K线选股系统】
 
@@ -48,12 +49,13 @@ class k:
             df = dl.load_data(self , code = code , start = start , end = end , ktype = ktype)
         return df
 
-    def new_high_break(self , MINIMUM = 5 ,MA_HIGH_PERIOD = 100 , auto_update = True) :
+    def new_high_break(self ,  code : str , start = None , end = None , ktype = "60" , MINIMUM = 3 ,MAXIMUM = 100 , MA_HIGH_PERIOD = 100 , auto_update = True) :
         """
         突破前高
         此函数设有过滤系统，必须突破新高次数降低到0以后重新回升才会计算在内；下降过程中的突破新高次数递减的情况会被过滤掉
         输入：
             MINIMUM：突破前高大于多少返回True 默认为3
+            MAXMUM：突破前高需要限定在多少以内 一般不会用到，先预留着，目前做到逻辑里面了，默认为100
             MA_HIGH_PERIOD 突破N均线 默认为100
                 注：通常日线采用20天突破 小时线采用100小时突破
             auto_update：是否将K线数据更新至最新 默认值：True （False则使用csv中的数据，不进行联网更新）
@@ -62,6 +64,25 @@ class k:
             True False
 
         """
+        #获取新高数据
+        df = self.k_new_high_count( code = code , start = start , end = end , ktype = ktype ,MA_HIGH_PERIOD = MA_HIGH_PERIOD , auto_update = auto_update)
+        #设置新高的趋势，所以新高次数逐渐增加，则设置1；逐渐减少设置-1；不变设置0
+        df['new_high_tendency'] = df['new_high_count'] - df['new_high_count'].shift(1)
+        #将新高趋势=0的调整为NA，再ffill NA，从而将1或者-1（既连续化上升或下降）
+        df.loc[df.new_high_tendency == 0 , 'new_high_tendency'] = np.nan
+        df.fillna(method='ffill' , inplace = True)
+        print(df[['code','new_high','new_high_tendency' ,'new_high_count']])
+        #print(df.loc[:,'new_high_count'])
+        if (df.iloc[-1].at['new_high_count'] >= MINIMUM) & (df.iloc[-1].at['new_high_count'] <= MAXIMUM) & (df.iloc[-1].at['new_high_tendency'] ==1):
+            #同时满足新高次数在上下限之间且非下降趋势（即回到0以后再上升的情况）
+            return True
+        else:
+            return False
+        print(df.iloc[-1].at['new_high_count'])
+        return df[['code','new_high','new_high_tendency','new_high_count']]
+
+
+
     def k_new_high_count(self , code : str , start = None , end = None , ktype = "60" , MA_HIGH_PERIOD = 100 , auto_update = True):
         """
         【计算K线中指定周期新高的次数】
@@ -78,9 +99,9 @@ class k:
         #小时线100小时最高收盘价计算
         df['MAHR_100_HIGH'] = df['high'].rolling(MA_HIGH_PERIOD).max()
         #计算此时点的最高是否为新高
-        df.loc[df.MAHR_100_HIGH == df.high,'new_high'] = 1    
+        df.loc[df.MAHR_100_HIGH == df.high , 'new_high'] = 1    
         #累计回滚新高
-        df = df.fillna(0)
+        df = df.fillna(0)  
         df['new_high_count'] = df['new_high'].rolling(MA_HIGH_PERIOD).sum()
         #数据错误风险提示
         if df.shape[0] <= MA_HIGH_PERIOD:
