b2c9c66ff5328b5cdf1eb660a3546178635d0d65
Hui Qiao <G.HUIQIAO@outlook.com>
2020-09-12 20:36:01 +0800

    完善多条件测试（新高突破+均线向上），给出一些结论性结果

--- demo/demo_multiple_condition.py
+++ demo/demo_multiple_condition.py
@@ -34,7 +34,7 @@ def load_today_all():
 if __name__=="__main__":
     #code_list= ['510300','510500','510050','510180','510900','159920','518880','159928','515030','512580','512170','512290','515220','515210','512720','515880','159995','159939','512760','512800','512880','512660','511010','511260','159949','512200','600089','600036','600519','600570','600958','300033','512200','300059','300236','603976','000651','601318','000063','159996','000001']
     code_list = load_today_all()
-    start = '2020-07-11'
+    start = '2020-06-11'
     end = '2020-09-11'
     ktype = '60'
     b =  k()
@@ -45,8 +45,9 @@ if __name__=="__main__":
     lst=[]
     for code in code_list:
         a = k()
-        if (a.new_high_break(code =code , start = start , end = end ,  ktype = ktype , MA_HIGH_PERIOD = 100) == True) and (a.ma_positive(code =code , start = start , end = end ,  ktype = ktype ) == True):
-            print("%s新高突破且均线向上" % (code))
-            lst.append(code)
+        #if (a.new_high_break(code =code , start = start , end = end ,  ktype = ktype , MA_HIGH_PERIOD = 100 ,auto_update = False) == True) and (a.ma_positive(code =code , start = start , end = end ,  ktype = ktype , auto_update = False) == True):
+        if a.ma_positive(code =code , start = start , end = end ,  ktype = ktype , auto_update = False) and a.new_high_break(code =code , start = start , end = end ,  ktype = ktype , MA_HIGH_PERIOD = 100 ,auto_update =False) :
+           print("%s新高突破且均线向上" % (code))
+           lst.append(code)
     print(lst)
     print(len(lst))
