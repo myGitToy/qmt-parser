0d20c89b20cecfdf298526715b72983815023426
Hui Qiao <G.HUIQIAO@outlook.com>
2020-09-17 23:20:46 +0800

    抽取auto_update至基类

--- apt/qsp/base.py
+++ apt/qsp/base.py
@@ -9,7 +9,7 @@ class base():
     """
     量化选股系统的基类
     """
-    def __init__(self , code = None , start = None , end = None , ktype = "D"):
+    def __init__(self , code = None , start = None , end = None , ktype = "D" , auto_update = True):
         """
         初始化
         输入：
@@ -17,6 +17,7 @@ class base():
             start：开始日期  e.g. yyyy-mm-dd
             end：结束日期    e.g. yyyy-mm-dd    
             ktype：K线类型 e.g. D 60 30
+            auto_update：数据自动更新 e.g. True False
         返回：
         """
         if code != None:
@@ -26,6 +27,7 @@ class base():
         self.start = start
         self.end = end
         self.ktype = ktype
+        self.auto_update = auto_update
 
     def get_rolling_k(self , ktype = "D"):
         """
@@ -48,12 +50,11 @@ class base():
             print("K线类型输入无效，按照默认日线数据返回结果")
             return 1
 
-
-    def get_k_data(self , auto_update = True):
+    def get_k_data(self):
         """
         抽取出来的总类，用于获取最新的K线数据，含自动更新
         输入：
-            auto_update：是否将K线数据更新至最新 默认值：True （False则使用csv中的数据，不进行联网更新）
+            auto_update：调用基类的值 是否将K线数据更新至最新 默认值：True （False则使用csv中的数据，不进行联网更新）
         返回：
             dataframe ：包含开盘 收盘 最高 最低 换手率（依据代码类型） 代码
             注：只返回基础数据，其他类似于MA ATR信息由其他函数进行计算
@@ -61,7 +62,7 @@ class base():
         #最后日期为空，则打开数据自动更新功能
         if  self.end == None:
             self.end = datetime.now().strftime("%Y-%m-%d")
-            auto_update = True
+            self.auto_update = True
         df = dl.load_data(self , code = self.code , start = self.start , end = self.end , ktype = self.ktype)
         #可能存在的一种情况：指定时间段内无数据，返回空dataframe
         if df.empty ==True:
@@ -69,7 +70,7 @@ class base():
         #两个日期序列化 last_index 为索引转换成日期，last_end为字符串转换成日期再按照指定格式输出
         last_index = df.last_valid_index().strftime( '%Y-%m-%d')
         last_end = datetime.strptime(self.end, '%Y-%m-%d').strftime( '%Y-%m-%d')
-        if (auto_update == True) & (last_index != last_end):
+        if (self.auto_update == True) & (last_index != last_end):
             #证券代码转换成列表格式
             lst=[]
             lst.append(self.code)
--- apt/qsp/vol.py
+++ apt/qsp/vol.py
@@ -11,30 +11,27 @@ from apt.qsp.base import base
 
 """
 class vol(base):
-
-
-    def amount_between(self ,  LOW = 0 , HIGH = 1e6 , x_rolling = 1 ,auto_update = True):
+    def amount_between(self , LOW = 0 , HIGH = 1e6 , x_rolling = 1 ):
         """
-        计算成交额是否在指定区间内
+        计算区间成交量
         HIGH LOW 成交额 1e8=1亿 1e6=100万
         小技巧：
             计算成交额小于N： LOW = 0 ; HIGH = N
             计算成交额大于N： LOW = N； HIGH = 1e12
         x_rolling 回溯K线数，默认回滚计算一天的K线 不同K线周期会采用不同的值，不建议自行调整
-        auto_update 自动更新 默认打开
         【返回值】 T/F
         """   
         #获取数据
-        df = self.get_k_data(auto_update)
+        df = self.get_k_data()
         #返回rolling数
         x_rolling = int(self.get_rolling_k(self.ktype))
         #截断函数，减少工作量
-        df = df.iloc[- (x_rolling + 20):]
+        df = df.iloc[-(x_rolling + 20):]
         #计算
-        df['cal_amount'] = df['close'] * df['volume'] *100
+        df['cal_amount'] = df['close'] * df['volume'] * 100
         df['rolling_amout'] = df['cal_amount'].rolling(x_rolling).sum()
-        print(df)
-        if (df.iloc[- 1].at['rolling_amout'] >= LOW) and ( df.iloc[- 1].at['rolling_amout'] <= HIGH ) :
+        #print(df)
+        if (df.iloc[-1].at['rolling_amout'] >= LOW) and ( df.iloc[-1].at['rolling_amout'] <= HIGH ) :
             return True
         else:
             return False
