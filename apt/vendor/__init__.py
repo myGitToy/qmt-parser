d88717e41f98ade7d6e737cf8222b0817a53f7e6
Hui Qiao <G.HUIQIAO@outlook.com>
2020-09-19 14:02:00 +0800

    tushare pro版的初始化（未全部完成）

---
+++ apt/vendor/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+【vendor系统】
+数据供应商系统
+目前只有tushare PRO一家，后续会逐渐添加
+
+
+
+"""
+from datetime import datetime,timedelta
+import numpy as np
+import pandas as pd
+import tushare as ts
+import logging
+import apt.os
+def print_tree_list():
+    print('''
+    根目录：
+    apt
+        -vendor #文档类主目录
+            -
+
+    
+    
+    ''')
