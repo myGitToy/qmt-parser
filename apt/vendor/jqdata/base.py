83f157864d9b999e5e847aa5974c23df1971de86
Hui Qiao <G.HUIQIAO@outlook.com>
2020-12-16 16:44:33 +0800

    增加Enum 交易时段校验 用于处理数据更新中涉及更新时段的逻辑

--- apt/vendor/jqdata/base.py
+++ apt/vendor/jqdata/base.py
@@ -1,9 +1,19 @@
 from jqdatasdk import *
 import sqlalchemy
+from enum import Enum
 class base():
     """
     jqdata中的一些基础数据
     """
+    class 交易时段校验(Enum):
+        """
+        检查当天日期是否是交易日和交易时段
+        非交易时段 定义为 交易日的非交易时段
+        """
+        非交易日 = 1
+        非交易时段 = 2
+        交易时段 = 3
+
     def __init__(self):
         auth('13817092632','JQ@tushare123') 
         self.engine = sqlalchemy.create_engine('mysql+pymysql://stock_user:a@1#Yy1c@localhost:3306/stock')
