27fb08e6ef8df8033ee87dcc312fe7d8da1157db
Hui Qiao <G.HUIQIAO@outlook.com>
2020-12-21 16:46:52 +0800

    增加jqdata.base初始化中对于数据源选择，目前连接字符串仅支持local host

--- apt/vendor/jqdata/base.py
+++ apt/vendor/jqdata/base.py
@@ -13,8 +13,28 @@ class base():
         非交易日 = 0
         非交易时段 = 1
         交易时段 = 2
+    class 数据源(Enum):
+        """
+        设置jqdata的数据源
+        """
+        aliyun = 0
+        aws = 1
+        localhost = 2
+        nas = 3
 
-    def __init__(self):
-        auth('13817092632','JQ@tushare123') 
-        self.engine = sqlalchemy.create_engine('mysql+pymysql://stock_user:a@1#Yy1c@localhost:3306/stock')
+    def __init__(self , rds_host = 数据源.localhost):
+
+        if rds_host == self.数据源.aliyun:
+            print("aliyun 暂不支持")
+        elif rds_host == self.数据源.aws:
+            print("aws 暂不支持")
+        elif rds_host == self.数据源.localhost:
+            self.engine = sqlalchemy.create_engine('mysql+pymysql://stock_user:a@1#Yy1c@localhost:3306/stock')
+            auth('13817092632','JQ@tushare123')
+        elif rds_host == self.数据源.nas:
+            print("nas 暂不支持")
+        else:
+            print("不支持的数据源，授权无效")
+        
+        
 
