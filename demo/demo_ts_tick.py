e3c8271766d4373ccba59ecc199cc6feb61e8328
Hui Qiao <G.HUIQIAO@outlook.com>
2020-12-26 22:36:37 +0800

    针对tick数据更新接口的变更，进行了相应的调整

--- demo/demo_ts_tick.py
+++ demo/demo_ts_tick.py
@@ -1,5 +1,7 @@
 from apt.os.data_tick import Data_tick
+import datetime
 
 a = Data_tick()
-a.daily_update()
+start = datetime.datetime(2020,12,16)
+a.update_v2(start_date = start)
 #df = a.get_tick_data(code = '000001' , day='2020-01-07')
