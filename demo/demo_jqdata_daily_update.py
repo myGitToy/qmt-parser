77fa754b5d94acb1d9ca204d9c3145adb22f0101
Hui Qiao <G.HUIQIAO@outlook.com>
2021-03-09 19:40:22 +0800

    get_k_data中增加一个参数 count 用于控制返回的数据量，比如要计算最后4条K线的数据，用start，end是无法实现这个功能的

--- demo/demo_jqdata_daily_update.py
+++ demo/demo_jqdata_daily_update.py
@@ -29,8 +29,8 @@ from apt.vendor.jqdata.ETF import ETF as ETF
 集合竞价数据	2019年至今	交易日最晚9:28分之前更新
 """
 
-start = datetime.datetime(2021,3,8)    #日线 60m 30m 最后更新日3/8 含
-                                         #5m 最后更新3/8含
+start = datetime.datetime(2021,3,9)    #日线 60m 30m 最后更新日3/9 含
+                                         #5m 最后更新3/9含
                                          #基金数据 最后更新3/6含
 #end = datetime.datetime(2018,12,31,16)
 end = datetime.datetime.now()
@@ -42,13 +42,13 @@ print(df_remain)
 #更新5分钟线
 #jq.update_v1(start_date = start , end_date = end , ktype = '5m' )
 #更新指数
-jq.update_index(start_date = start , end_date = end , ktype = '5m')
-jq.update_index(start_date = start , end_date = end , ktype = '30m')
-jq.update_index(start_date = start , end_date = end , ktype = '60m')
-jq.update_index(start_date = start , end_date = end , ktype = '1d')
+#jq.update_index(start_date = start , end_date = end , ktype = '5m')
+#jq.update_index(start_date = start , end_date = end , ktype = '30m')
+#jq.update_index(start_date = start , end_date = end , ktype = '60m')
+#jq.update_index(start_date = start , end_date = end , ktype = '1d')
 
 #更新日线
-jq.update_v2(start_date = start , end_date = end , ktype = '1d' )
+#jq.update_v2(start_date = start , end_date = end , ktype = '1d' )
 #更新60分钟线
 jq.update_v2(start_date = start , end_date = end , ktype = '60m' )
 #更新30分钟线
