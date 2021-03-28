1232980140cc37d9730119e1acbd944182f71063
Hui Qiao <G.HUIQIAO@outlook.com>
2021-03-28 14:07:21 +0800

    daily update

--- demo/demo_jqdata_daily_update.py
+++ demo/demo_jqdata_daily_update.py
@@ -29,9 +29,10 @@ from apt.vendor.jqdata.ETF import ETF as ETF
 集合竞价数据	2019年至今	交易日最晚9:28分之前更新
 """
 
-start = datetime.datetime(2021,3,19)    #日线 60m 30m 最后更新日3/16含
-                                         #5m 最后更新3/16含
-                                         #基金数据 最后更新3/6含
+start = datetime.datetime(2021,3,26)    #日线 60m 30m 最后更新日3/25含 
+             #（注意：日线数据不能在过零点及开盘前更新，否则会出现类似于停盘的数据 无VOL MONEY）
+                                         #5m 最后更新3/25含
+                                         #基金数据 最后更新3/22不含
 #end = datetime.datetime(2018,12,31,16)
 end = datetime.datetime.now()
 jq = jqdata(rds_host = jqdata.数据源.localhost , myauth = True )
@@ -39,6 +40,9 @@ df_remain = get_query_count()
 print(df_remain)
 #dd = get_bars('399001.XSHE', end_dt = '2009-11-25',count =24,unit='5m' , fields = ['date', 'open', 'close', 'high', 'low', 'volume', 'money','factor'])
 #print(dd)
+#更新ETF数据
+etf = ETF()
+#etf.update_fund_share_daily(start_date = start)
 #更新5分钟线
 jq.update_v1(start_date = start , end_date = end , ktype = '5m' )
 #更新指数
@@ -53,6 +57,3 @@ jq.update_v2(start_date = start , end_date = end , ktype = '1d' )
 jq.update_v2(start_date = start , end_date = end , ktype = '60m' )
 #更新30分钟线
 jq.update_v2(start_date = start , end_date = end , ktype = '30m' )
-#更新ETF数据
-etf = ETF()
-#etf.update_fund_share_daily(start_date = start)
