2cfea1980a2e8e8a135d7ccbc9b69221f170a995
Hui Qiao <G.HUIQIAO@outlook.com>
2021-03-31 23:52:48 +0800

    增加资金流向的日常更新功能money_flow.daily_update

--- demo/demo_jqdata_money_flow.py
+++ demo/demo_jqdata_money_flow.py
@@ -10,19 +10,21 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 auth('13817092632','JQ@tushare123')
 
-start = datetime.datetime(2020,11,12)
-end = datetime.datetime(2021,1,13   )
+start = datetime.datetime(2020,10,1)
+end = datetime.datetime.now()
 print(start)
 print(start.date())
-code = '600519.XSHG'
+code = '000063.XSHE'
 #df_share = finance.run_query(query(finance.FUND_SHARE_DAILY).filter(finance.FUND_SHARE_DAILY.date >= '2020/12/8' , finance.FUND_SHARE_DAILY.date <= '2020/12/10' , finance.FUND_SHARE_DAILY.code == '159949.XSHE' ))
 #print(df_share)
 #获取全部
 #df = get_money_flow(['300142.XSHE'], start_date='2020-6-16', end_date='2020-9-16', fields=None, count=None)
 #获取大单date     sec_code  change_pct  net_amount_main  net_pct_main
 df = get_money_flow([code] , start_date = start, end_date = end , fields = ['date','change_pct','net_amount_main','net_pct_main'] , count = None)
-df['net_pct_main_ma']=df['net_amount_main'].rolling(5).mean()
-print(df)
+df['ma5']=df['net_amount_main'].rolling(5).mean()
+df['ma14']=df['net_amount_main'].rolling(14).mean()
+df['code'] = code
+print(df[['date','code','net_amount_main','ma5','ma14']])
 
 
 
