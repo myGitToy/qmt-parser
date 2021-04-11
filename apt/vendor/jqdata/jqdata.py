b520e6218a174eca198186c0404ac1ada94372a2
Hui Qiao <G.HUIQIAO@outlook.com>
2021-04-11 22:32:47 +0800

    对read_excel模块进行合并，删除demo文档，合并注释

--- apt/vendor/jqdata/jqdata.py
+++ apt/vendor/jqdata/jqdata.py
@@ -425,6 +425,12 @@ class data(base):
     def read_excel(file_name = None , sheet_name = None):
         """
         读取excel数据
+        注意事项：
+            1. 默认引擎为xlrd，目前高版本已不支持xlsx文件
+            2. 切换引擎至openpyxl 
+            3. 上述两个引擎都需要额外pip install
+            4. 表的名称和列的名称目前都支持中文
+            5. 读取时excel表格必须处于关闭状态，否则会报错（已通过增加exception做到提示错误信息）
         """
         try:
             df = pd.read_excel( file_name, sheet_name = sheet_name , engine = 'openpyxl' )
