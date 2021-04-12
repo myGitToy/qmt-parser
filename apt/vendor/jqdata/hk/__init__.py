e9d09dec140666d2b6d1ed589735fe9969298ab9
Hui Qiao <G.HUIQIAO@outlook.com>
2021-04-12 17:58:08 +0800

    增加市场通模块树

---
+++ apt/vendor/jqdata/hk/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+【市场通数据】
+STK_AH_PRICE_COMP 市场通AH股价格对比 / 上市至今，交易日20:30-06:30更新
+STK_EL_CONST_CHANGE 市场通合格证券变动记录 / 上市至今，交易日20:30-06:30更新
+STK_EXCHANGE_LINK_CALENDAR 市场通交易日历 / 上市至今，交易日20:30-06:30更新
+STK_EL_TOP_ACTIVATE 市场通十大成交活跃股 / 上市至今，交易日20:30-06:30更新
+STK_ML_QUOTA 市场通成交与额度信息 / 上市至今，交易日20:30-06:30更新
+STK_EXCHANGE_LINK_RATE 市场通汇率 / 上市至今，交易日20:30-06:30更新
+STK_HK_HOLD_INFO 沪深港通持股数据 / 上市至今，交易日20:30-06:30更新
+"""
+
+
+def print_tree_list():
+    print('''
+    根目录：
+    apt
+        -vendor #文档类主目录
+            -
+
+    
+    
+    ''')
---
+++ apt/vendor/jqdata/hk/stk_hk_hold_info.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+import tushare as ts
+import sqlalchemy
+import datetime
+from jqdatasdk import *
+#from apt.vendor.jqdata.stk import stk as stk
+
+class stk_hk_hold_info():
+    """
+    STK_HK_HOLD_INFO 沪深港通持股数据 / 上市至今，交易日20:30-06:30更新
+    """
+    def daily_update(self , code_list =  None , start_date = datetime.datetime(2005,1,1) , end_date = datetime.datetime.now()):
+        """
+        沪深港通持股数据
+        """
+        #获取更新列表
+        if code_list == None:
+            #更新列表未填写，则默认为空，即全部更新
+            code_list = list(get_all_securities(['stock'],date = end_date).index)
+        #打印标题
+        print("############正在准备更新资金流向信息###########")
+        """
+        更新逻辑：
+            1. 取出所有的stock列表，进行单代码循环
+            2. 取出该代码在数据库中，指定日期间的最后更新日期
+                2.1 为空，则全部更新
+                2.2 不为空，则取出最后日期，以这个日期为基准，进行更新
+            3. 数据写入数据库
+        逻辑优点和不足：
+            1. 单循环运行效率较高
+            2. 可以解决日常更新中，有部分日期重复的问题，比如每月的更新开始周期设定为1号，但5号更新是会自动过滤1-4号的数据
+            3. 上述第二条还有一个很重要的应用场景：因为是指定区间的更新取最后一天，因此如果先更新了2020年的数据，随后再要更新2019年的数据，
+                在这种情况下也是可以的，因为取的是区间最后一天，因此2019全年的数据也是空，也就会全部读取和更新
+            4. 这里写入时没有做去重，可能还是会触发唯一性约束错误（可能性低于0.01%）
+        """
+        for code in code_list:
+            #获取数据库中存在的数据最后更新日期（加入强制使用索引的内容）
+            query2 = f"select date FROM jqdata_money_flow FORCE INDEX (main) WHERE date BETWEEN '{start_date.date()}' and '{end_date.date()}' and code = '{code}' ORDER BY date DESC LIMIT 1" 
+            df_db = pd.read_sql_query(query2 , self.engine)
+            if df_db.empty == True:
+                #数据库不存在数据
+                new_start_date = start_date
+            else:
+                #数据库存在数据，新定义开始日期（数据库最后一天+1）
+                new_start_date =  df_db.loc[0 , 'date']  + datetime.timedelta(days=1)         
+            df_flow = get_money_flow(security_list = code , start_date = new_start_date, end_date = end_date)
+            #重命名列（money_flow中的证券代码sec_code和数据库中的不一致）
+            df_flow.rename(columns = {'sec_code':'code'} , inplace = True)
+            #print(df_flow)
+            #保存至数据库 
+            if df_flow.empty == True:
+                print("%s 进行差集处理后剩余数据为空或者jqdata无数据，跳过上传" % (code))
+            else:
+                df_flow.to_sql(
+                        name = 'jqdata_money_flow',
+                        con = self.engine,
+                        index = False,
+                        if_exists = 'append')
+                print("%s 数据已上传完成(money flow)" % (code))
+    def delete_null(self):
+        """
+        负责处理异常数据
+        https://huiqiao.visualstudio.com/MyFunds/_boards/board/t/MyFunds%20Team/Backlog%20items/?workitem=204
+
+        """
+        query2 = f"delete from jqdata_money_flow where (net_pct_s is null or net_amount_s is NULL) and date >='2021/1/1'" 
+        df_db = pd.read_sql_query(query2 , self.engine)
+        #print(df_db)
+if __name__=="__main__":
+    #此模块用于历史数据的更新，目前2021年前的数据已完成更新，因此模块下架停止使用
+    money = money_flow()
+    start = datetime.datetime(2021,1,1)
+    end = datetime.datetime.now() #2020年数据已完成更新
+    df_remain = get_query_count()
+    print(df_remain)
+    money.delete_null()
+    money.daily_update( start_date = start , end_date =end)
+    df_remain = get_query_count()
+    print(df_remain)
