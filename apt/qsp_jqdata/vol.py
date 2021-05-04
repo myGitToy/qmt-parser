dcc9f172ea2c0a2b071bb0852122876cd9e6cd1e
Hui Qiao <G.HUIQIAO@outlook.com>
2021-05-04 16:32:17 +0800

    更改成交量计算的逻辑和参数，加入N均值参数

--- apt/qsp_jqdata/vol.py
+++ apt/qsp_jqdata/vol.py
@@ -10,14 +10,18 @@ import pandas as pd
 
 """
 class vol(base):
-    def amount_between(self , LOW = 0 , HIGH = 1e6 , x_rolling = 1 ):
+    def money_between(self , LOW = 0 , HIGH = 1e6 , x_rolling = 1 , N = 1):
         """
-        计算区间成交量
-        HIGH LOW 成交额 1e8=1亿 1e6=100万
+        计算区间成交金额
+        HIGH LOW 成交额 1e8 = 1亿 1e6 = 100万
+        N 需要计算N天的平均值（因为成交量存在忽大忽小的情况）
+            默认值是计算当天的，即N = 1
         小技巧：
             计算成交额小于N： LOW = 0 ; HIGH = N
             计算成交额大于N： LOW = N； HIGH = 1e12
-        x_rolling 回溯K线数，默认回滚计算一天的K线 不同K线周期会采用不同的值，不建议自行调整
+        x_rolling ：比如60m就需要计算连续4天的值做汇总，否则数据是不准确的
+        【例】money_between(LOW = 12e8 , HIGH= 1e12 , N = 3)
+            计算某代码在3天内的平均成交量是否在12亿以上
         【返回值】 T/F
         """   
         #获取数据
@@ -25,12 +29,11 @@ class vol(base):
         #返回rolling数
         x_rolling = int(self.get_rolling_k(self.ktype))
         #截断函数，减少工作量
-        df = df.iloc[-(x_rolling + 20):]
+        df = df.iloc[-(x_rolling * N + 20):]
         #计算
-        df['cal_amount'] = df['close'] * df['volume'] * 100
-        df['rolling_amout'] = df['cal_amount'].rolling(x_rolling).sum()
+        df['rolling_money'] = df['money'].rolling(x_rolling * N ).sum() / N
         #print(df)
-        if (df.iloc[-1].at['rolling_amout'] >= LOW) and ( df.iloc[-1].at['rolling_amout'] <= HIGH ) :
+        if (df.iloc[-1].at['rolling_money'] >= LOW) and ( df.iloc[-1].at['rolling_money'] <= HIGH ) :
             return True
         else:
             return False
