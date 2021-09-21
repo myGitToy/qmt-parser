9832ae32e3418d58deb3f9ba33ad950f9ed54da5
Hui Qiao <G.HUIQIAO@outlook.com>
2021-09-21 11:09:54 +0800

    增加证券代码security的更新写入功能，建立基础模块

---
+++ apt/vendor/jqdata/security.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import tushare as ts
+import sqlalchemy
+import datetime
+from jqdatasdk import *
+from apt.vendor.jqdata.base import base as base
+
+class security(base):
+    """
+    专门处理security的类
+    """
+    def daily_update(self , type = ['stock','index','fund','etf','lof','fja','fjb']):
+        """
+        security日常更新
+        type 证券类型，支持多选 默认为全部（目前不包含期货）
+        """
+        #打印标题
+        print("############正在准备更新security证券代码信息###########")
+        df_security = get_all_securities(type)
+        #将code的索引转换成列
+        df_security.reset_index(level = 0 , inplace = True)
+        #重命名为code
+        df_security.rename(columns = {'index':'code'} , inplace = True)
+        #print(df_security)
+        #数据保存至数据库
+        if df_security.empty == True:
+            print("原始数据为空，无法导入数据库")
+        else:
+            df_security.to_sql(
+                    name = 'jqdata_security',
+                    con = self.engine,
+                    index = False,
+                    if_exists = 'replace')
+            print("数据已上传完成(security)")
