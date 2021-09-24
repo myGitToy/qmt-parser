84612c97b5efdc53314cda855a71ec1418829742
Hui Qiao <G.HUIQIAO@outlook.com>
2021-09-24 21:28:29 +0800

    增加导入数据库后重新设置主键的功能

--- apt/vendor/jqdata/security.py
+++ apt/vendor/jqdata/security.py
@@ -5,6 +5,7 @@ import sqlalchemy
 import datetime
 from jqdatasdk import *
 from apt.vendor.jqdata.base import base as base
+from sqlalchemy.types import NVARCHAR , Float, Integer , Date
 
 class security(base):
     """
@@ -23,6 +24,15 @@ class security(base):
         #重命名为code
         df_security.rename(columns = {'index':'code'} , inplace = True)
         #print(df_security)
+        #dataframe列名的数据类型进行映射
+        dtypedict = {
+            'code': NVARCHAR(length = 24),
+            'display_name': NVARCHAR(length = 128),
+            'name': NVARCHAR(length = 128),
+            'start_date': Date(),
+            'end_date': Date(),
+            'type': NVARCHAR(length = 8)
+                    }
         #数据保存至数据库
         if df_security.empty == True:
             print("原始数据为空，无法导入数据库")
@@ -31,5 +41,10 @@ class security(base):
                     name = 'jqdata_security',
                     con = self.engine,
                     index = False,
-                    if_exists = 'replace')
+                    if_exists = 'replace',
+                    index_label='code' , #设置主键(设置未成功)
+                    dtype=dtypedict) #映射列的数据类型
+            #设置主键
+            with self.engine.connect() as con:
+                con.execute('ALTER TABLE `jqdata_security` ADD PRIMARY KEY (`code`);')
             print("数据已上传完成(security)")
