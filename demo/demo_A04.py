e448bdf5a192d159d255d9422d0b6f88cc97e043
Hui Qiao <G.HUIQIAO@outlook.com>
2021-11-23 22:36:45 +0800

    增加函数A04B06_EMA均线_线性回归角度 目前主要用于判断是否处于平台整理

--- demo/demo_A04.py
+++ demo/demo_A04.py
@@ -10,13 +10,16 @@ import talib as ta
 
 #from jqlib.technical_analysis import *
 a= A()
-a.code = '603259.XSHG'
+a.code = '601318.XSHG'
 a.start = datetime(2021,1,1)
-a.end = datetime(2021,11,20)
+a.end = datetime(2021,10,26)
 a.myauth = False
 #df = a.A04B01_EMA均线数据()
 #测试A04B04
-df = a.A04B05_EMA均线_收盘价小于均线(ma = '120', adjust_N = 10 , count = 10)
+#df = a.A04B05_EMA均线_收盘价小于均线(ma = '120', adjust_N = 10 , count = 10)
+
+#测试A04B06
+df = a.A04B06_EMA均线_线性回归角度(ma = '20', adjust_N = 20 , count = 16)
 print(df)
 
 #测试均线多头排列
