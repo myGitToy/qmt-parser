309588ee272a4f73850eeff63d5956ff73a959bb
Hui Qiao <G.HUIQIAO@outlook.com>
2021-11-29 16:13:44 +0800

    增加自定义服务器的功能，默认使用local host

--- bt/Data.py
+++ bt/Data.py
@@ -1,7 +1,15 @@
-from apt.vendor.jqdata.jqdata import data as jqdata
+from apt.vendor.jqdata.jqdata import data as jqdata #jqdata的数据服务基类
+from apt.vendor.jqdata.base import base as bb   #最基础的基类，用于设定服务器和授权信息
 #from apt.vendor.jqdata.base import base as base
-from apt.qsp_jqdata.base import base
+from apt.qsp_jqdata.base import base    #数据分析的基类
 class Data(base):
+    def init(self , server = None):
+        #bt.Data的自定义设置，除非特殊指定，否则数据源默认为localhost
+        if server == None :
+            self.server = bb.数据源.localhost
+        else:
+            self.server = server
+
     def get_bt_data(self):
         '''
         获取Backtrader所需使用的基础数据，使用父类的get_k_data
@@ -12,5 +20,9 @@ class Data(base):
         df = a.jqdata_to_backtrader(df)
         return df
         print(df)
+
+
+
+
 
 
