3ad57735ea15a83db0f97f1b3d992e4deaca8900
Hui Qiao <G.HUIQIAO@outlook.com>
2021-11-29 10:27:34 +0800

    demo测试

--- demo/demo_tbl_斜率角.py
+++ demo/demo_tbl_斜率角.py
@@ -15,19 +15,23 @@ pd.set_option('display.max_rows', None)
 
 #from jqlib.technical_analysis import *
 a= A()
-a.code = '300041.XSHE'
+a.code = '515880.XSHG'
 a.start = datetime(2021,1,1)
-a.end = datetime(2021,11,23)
+a.end = datetime(2021,11,24)
 a.myauth = False
 #df = a.A04B01_EMA均线数据()
 #获取EMA均线数据
 df = a.A04B01_EMA均线数据()
+#数据归一化 DataScaling
+min = df['EMA20'].min()
+max = df['EMA20'].max()
+df['scale'] = (df['EMA20'] - min)/(max - min)
+#print(df['scale'] )
 #talib库提供LINEARREG_ANGLE函数用以计算线性回归的角度，LINEARREG_SLOPE函数用以计算线性回归斜率
 #基于EMA20 timeperiod=2的数据研究，3以内数据平台整理，出现大于5开始有突破动作
 #用period2比较理想，其他数据均有较大延迟，建议使用N天内出现X次的思路
-#df['EMA_ANGLE'] = ta.LINEARREG_ANGLE(df['EMA20'] , timeperiod = 2)
-df['TSF'] = ta.TSF(df['EMA20'] , timeperiod = 20)
-df['EMA_SLOPE'] =(df['EMA20'] - df[f'EMA20'].shift(1)) / df[f'EMA20'].shift(1)
+df['EMA_ANGLE'] = ta.LINEARREG_ANGLE(df['scale'] , timeperiod = 20)
+#df['TSF'] = ta.TSF(df['EMA20'] , timeperiod = 20)
+#df['EMA_SLOPE'] =(df['EMA20'] - df[f'EMA20'].shift(1)) / df[f'EMA20'].shift(1)
 #df['x_s'] = ta.LINEARREG_SLOPE(df['EMA20'] , timeperiod = 2)
-print(df)
-#print(df[['date','code','close','EMA20','EMA_ANGLE','x_s']])
+print(df[['date','code','close','EMA20','EMA_ANGLE','scale']])
