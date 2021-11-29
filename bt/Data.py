57f1f595a54279f300ad037e91386e76209e3330
Hui Qiao <G.HUIQIAO@outlook.com>
2021-11-29 17:20:11 +0800

    bt模块中获取交易日历（直接调用基类的方法）

--- bt/Data.py
+++ bt/Data.py
@@ -14,14 +14,20 @@ class Data(base):
         '''
         获取Backtrader所需使用的基础数据，使用父类的get_k_data
         '''
-        a = jqdata(myauth = self.myauth)
+        a = jqdata(rds_host = self.server , myauth = self.myauth)
         #此处的get_k_data从vendor.jqdata.data.get_k_data取数据，非tushare
         df = a.get_k_data(code = self.code , start_date = self.start , end_date = self.end , ktype = self.ktype , fq = self.fq)
         df = a.jqdata_to_backtrader(df)
         return df
         print(df)
 
-
+    def get_trader_days(self):
+        """
+        获取交易日历，从基类中读取       
+        """
+        a = jqdata(rds_host = self.server , myauth = self.myauth)
+        df = a.get_trader_days(start_date = self.start , end_date = self.end)
+        return df
 
 
 
