f1ab3ed5ae85f58a684a3d6e0590d605e6204a37
Hui Qiao <G.HUIQIAO@outlook.com>
2022-05-20 14:56:20 +0800

    将原先仅提供T/F值的函数改造成了返回多种数据类型的函数 增加interval区间内只计算第一次出现信号，后续一段时间内持续出现的信号做忽略处理

--- apt/qsp_jqdata/vol.py
+++ apt/qsp_jqdata/vol.py
@@ -38,37 +38,61 @@ class vol(base):
         else:
             return False
 
-    def money_abnormal_change(self , vol_ma = 20 , criteria = 2 , N = 5 , count = 1 ):
+    def money_abnormal_change(self , vol_ma = 20 , criteria = 2 , N_day = 5 , count = 1 , interval = 1 ):
         """
         计算区间是否存在成交量异动
         vol_ma 需要回滚ma根均线的成交量均值作为比较基准，默认是20天均值
         criteria 异动的标准 默认为均值ma的两倍
         N 几天内出现这种情况算是符合条件 默认为一周内，即5天。如需计算当天的情况，则N = 1
         count N周期内出现几次算符合条件 count <= N
-        【例】money_abnormal_change(range = 20 ,criteria = 3 , N = 3 , count = 2 )
+        【重要提示】
+            成交量异常的判断和K线类型是挂钩的，日线以下级别由于当天开盘前30分钟成交量的影响，会干扰选股结果
+            目前不对K线类型做限制，但是需要使用者清楚明白分时线可能带来的误差
+        【例】money_abnormal_change(vol_ma = 20 ,criteria = 3 , N = 3 , count = 2 )
             计算某代码是否在3天内有2次超过平均成交量3倍以上的情况
-        【返回值】 T/F
-        暂存，已无用 DataFrame：证券代码 日期 成交量异动？？？？？暂定 目前不确定  也可能是T/F形式
+            用文字表达就是A股票在某日，存在N_day内存在count次成交量大于vol_ma平均成交量criteria次的情况
+        【返回值】 元组
+            0列：DataFrame 包含日期 证券代码 结果
+            1列：T/F 只包含最后交易日是否满足条件的判断
         """   
         #数据校验
-        if count > N:
+        if count > N_day:
             raise ValueError(f'无法计算{N}天内出现{count}次的情况，请检查逻辑')
+        #k线类型校验（推荐但不报错）
+        if self.ktype != "1d":
+            print("使用了不推荐的K线类型，返回结果不保证精确度")
         #获取数据
         df = self.get_k_data()
         if df.empty == True:
             raise ValueError(f'数据为空，请检查！')
         #返回rolling数
         x_rolling = int(self.get_rolling_k(self.ktype))
-        #截断函数，减少工作量
-        df = df.iloc[-(x_rolling * vol_ma + 20):]
+        #截断函数，减少工作量（取消截断，因为后续要改为DataFrame返回输出，因此需要计算全部数据行）
+        #df = df.iloc[-(x_rolling * vol_ma + 20):]
         #计算
         df['rolling_money'] = df['money'].rolling(x_rolling * vol_ma ).sum() / vol_ma
         df['m_div_ma'] = df['money'] / df['rolling_money']
+        #增加[abnormal]列
+        #含义：满足成交量放大条件的交易日，设置abnormal = 1
         df.loc[df['m_div_ma'] >= criteria , 'abnormal'] = 1
         df = df.fillna(0)
-        df['abnormal_amout'] = df['abnormal'].rolling(count).sum()
-        #print(df)        
-        if df.iloc[-1].at['abnormal_amout'] >= count :
-            return True
+        #增加['abnormal_amout']列
+        #含义：连续N_day天内满足成交量放大的天数，最后会用这个天数和count进行比较
+        df['abnormal_amout'] = df['abnormal'].rolling(N_day).sum()
+        #增加[result-1]列
+        #含义：作为最后结果的前置列，输出0/1值，含义是是否满足count的要求
+        df.loc[df['abnormal_amout'] >= count , 'result-1'] = 1
+        df = df.fillna(0)
+        #增加[result]列
+        #含义：最后的输出结果，对interval进行判断，如果X天内重复计算则去除第二个以后的结果
+        df['result'] = df['result-1'].rolling(interval).sum() * df['result-1']
+        df.loc[df['result'] > 1 , 'result'] = 0
+        #校验结果，输出全部列（仅用于测试，正常情况下注释掉）
+        #print(df)
+        #返回DataFrame中最后一行是否符合条件，这个返回值属于传统的T/F值
+        if df.iloc[-1].at['abnormal_amout'] >= count :#此处不涉及到interval判断，仅判断最后一天是否符合条件
+            last_row = True
         else:
-            return False
+            last_row = False
+        #返回最后的结果 目前为元组类型，第一列为DataFrame 第二列为T/F值       
+        return df[['code','date','result']] , last_row    
--- demo/demo_vol_test.py
+++ demo/demo_vol_test.py
@@ -7,29 +7,32 @@ from datetime import datetime
 from apt.vendor.jqdata.security import security as info
 from apt.vendor.jqdata.finance.finance_valuation import finance_valuation as valuation
 from apt.vendor.jqdata.money_flow import money_flow as money
-
-a = vol(code =  '002352.XSHE' , start = datetime(2021,5,1) , end = datetime(2021,9,16) , ktype = '1d' , myauth = False)
+pd.set_option('display.max_rows', None)
+#a = vol(code =  '600313.XSHG' , start = datetime(2021,5,1) , end = datetime(2021,9,16) , ktype = '1d' , myauth = False)
 t = atr(code =  '002709.XSHE' , start = datetime(2021,5,1) , end = datetime(2021,9,20) , ktype = '1d' , myauth = False)
 inf = info(myauth = False)
 print(1e2 / 100)
+
+a=vol()
 #信息初始化
-#a.code = '002352.XSHE'
-#a.start = '2021/5/1'
-#a.end = '2021/9/14'
-#a.ktype = '1d'
-#a.myauth = False
+a.code = '600313.XSHG'
+a.start = '2022/1/1'
+a.end = '2022/3/30'
+a.ktype = '1d'
+a.myauth = False
 
 #vol条件筛选
 #vol_amt = a.money_between(LOW=0 ,HIGH =1982110598)
 #print(vol_amt)
-m = money(myauth = False)
+#m = money(myauth = False)
 
-m.get_money_flow(code = '688002.XSHG')
+#m.get_money_flow(code = '688002.XSHG')
 
 
 #vol异常值筛选
-vol_abnormal = a.money_abnormal_change(criteria = 2 ,count = 4 , N=4)
-print(vol_abnormal)
+vol_abnormal = a.money_abnormal_change(vol_ma = 20 , criteria = 2 ,count = 3 , N_day = 8 , interval = 8)
+df2 = vol_abnormal[1]
+print(df2)
 
 dd = data(myauth = False)
 dd.get_all_code(end_date =  datetime(2021,9,23) , local = True)
