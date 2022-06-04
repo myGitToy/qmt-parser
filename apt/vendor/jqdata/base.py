d5e95574eb8e5b1b91b92d70b23f8062b97bd8b5
Hui Qiao <G.HUIQIAO@outlook.com>
2022-06-04 16:50:59 +0800

    增加新账号授权信息

--- apt/vendor/jqdata/base.py
+++ apt/vendor/jqdata/base.py
@@ -44,21 +44,21 @@ class base():
         self.myauth = myauth
         if rds_host == self.数据源.aliyun:
             print("aliyun 暂不支持")
-            auth('18621899367','Qq19840207')
+            auth('13162818663','Qq@6537286')
         elif rds_host == self.数据源.aws:
             #database-1.cluster-czherlzuxybq.us-west-2.rds.amazonaws.com
             self.engine = sqlalchemy.create_engine('mysql+pymysql://stock_user:a1#Yy1cTc@database-1.cluster-czherlzuxybq.us-west-2.rds.amazonaws.com:3306/stock')
             #RDS数据库采用Amazon Aurora MySQL Serverless
             if myauth == True:
-                auth('18621899367','Qq19840207')
+                auth('13162818663','Qq@6537286')
         elif rds_host == self.数据源.localhost:
             self.engine = sqlalchemy.create_engine('mysql+pymysql://stock_user:a@1#Yy1c@localhost:3306/stock')
             #本地数据源支持脱机访问，其他数据源则不支持脱机
             if myauth == True:
-                auth('18621899367','Qq19840207')
+                auth('13162818663','Qq@6537286')
         elif rds_host == self.数据源.nas:
             print("nas 暂不支持")
-            auth('18621899367','Qq19840207')
+            auth('13162818663','Qq@6537286')
         else:
             print("不支持的数据源，授权无效")
         
