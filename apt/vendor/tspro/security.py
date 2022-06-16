2a293264d46bc80fb31a8ee399bbffac5b5cfe15
Hui Qiao <G.HUIQIAO@outlook.com>
2022-06-16 17:00:25 +0800

    顺序调整 无用代码删除

--- apt/vendor/tspro/security.py
+++ apt/vendor/tspro/security.py
@@ -49,77 +49,6 @@ class security(base , stock):
                     if_exists = 'append')
             print(f"数据已上传完成(交易日历)，新增数据{df_diff.shape[0]}条")
 
-    def get_security(self , code = None , day = datetime.now()):
-        """
-        获取单代码的security信息（需要满足）
-        【输入】
-            code 证券代码 默认为空
-        【输出】
-            dataframe:code|display_name|name|start_date|end_date|type|valid
-        """
-        if code != None:
-            #脱机查询
-            #定位数据库中的最后日期(这里默认使用510300进行查询)
-            df = pd.read_sql_query(f"select * from jqdata_security where code = '{code}' and start_date<='{day.date()}' and end_date >='{day.date()}'" , self.engine)
-            if df.empty == True:
-                #数据库不存在数据
-                return pd.DataFrame()
-            else:
-                #数据库存在数据，返回
-                return df
-        else:
-            raise ValueError(f'证券代码不能为空，请检查')
-
-
-    def daily_update_money_flow(self , code_list =  None , start_date = datetime(2005,1,1) , end_date = datetime.now()):
-        """
-        每日资金流向更新
-        """
-        #获取更新列表
-        if code_list == None:
-            #更新列表未填写，则默认为空，即全部更新
-            code_list = list(get_all_securities(['stock'],date = end_date).index)
-        #打印标题
-        print("############正在准备更新资金流向信息###########")
-        """
-        更新逻辑：
-            1. 取出所有的stock列表，进行单代码循环
-            2. 取出该代码在数据库中，指定日期间的最后更新日期
-                2.1 为空，则全部更新
-                2.2 不为空，则取出最后日期，以这个日期为基准，进行更新
-            3. 数据写入数据库
-        逻辑优点和不足：
-            1. 单循环运行效率较高
-            2. 可以解决日常更新中，有部分日期重复的问题，比如每月的更新开始周期设定为1号，但5号更新是会自动过滤1-4号的数据
-            3. 上述第二条还有一个很重要的应用场景：因为是指定区间的更新取最后一天，因此如果先更新了2020年的数据，随后再要更新2019年的数据，
-                在这种情况下也是可以的，因为取的是区间最后一天，因此2019全年的数据也是空，也就会全部读取和更新
-            4. 这里写入时没有做去重，可能还是会触发唯一性约束错误（可能性低于0.01%）
-        """
-        for code in code_list:
-            #获取数据库中存在的数据最后更新日期（加入强制使用索引的内容）
-            query2 = f"select date FROM jqdata_money_flow FORCE INDEX (main) WHERE date BETWEEN '{start_date.date()}' and '{end_date.date()}' and code = '{code}' ORDER BY date DESC LIMIT 1" 
-            df_db = pd.read_sql_query(query2 , self.engine)
-            if df_db.empty == True:
-                #数据库不存在数据
-                new_start_date = start_date
-            else:
-                #数据库存在数据，新定义开始日期（数据库最后一天+1）
-                new_start_date =  df_db.loc[0 , 'date']  + datetime.timedelta(days=1)         
-            df_flow = get_money_flow(security_list = code , start_date = new_start_date, end_date = end_date)
-            #重命名列（money_flow中的证券代码sec_code和数据库中的不一致）
-            df_flow.rename(columns = {'sec_code':'code'} , inplace = True)
-            #print(df_flow)
-            #保存至数据库 
-            if df_flow.empty == True:
-                print(f"{code}数据为空，跳过上传")
-            else:
-                df_flow.to_sql(
-                        name = 'jqdata_money_flow',
-                        con = self.engine,
-                        index = False,
-                        if_exists = 'append')
-                print("%s 数据已上传完成(money flow)" % (code))
-
     def get_calendar(self , exchange = 'SSE' , is_open = 1 , all = False):
         """
         获取交易日历
@@ -149,7 +78,29 @@ class security(base , stock):
         else:
             #数据库存在数据
             return df_db
-                
+ 
+    def get_security(self , code = None , day = datetime.now()):
+        """
+        获取单代码的security信息（需要满足）
+        【输入】
+            code 证券代码 默认为空
+        【输出】
+            dataframe:code|display_name|name|start_date|end_date|type|valid
+        """
+        if code != None:
+            #脱机查询
+            #定位数据库中的最后日期(这里默认使用510300进行查询)
+            df = pd.read_sql_query(f"select * from jqdata_security where code = '{code}' and start_date<='{day.date()}' and end_date >='{day.date()}'" , self.engine)
+            if df.empty == True:
+                #数据库不存在数据
+                return pd.DataFrame()
+            else:
+                #数据库存在数据，返回
+                return df
+        else:
+            raise ValueError(f'证券代码不能为空，请检查')
+
+               
 if __name__=="__main__":
     #测试交易日历功能
     cal = security()
