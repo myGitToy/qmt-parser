ef731699153fba5d6e383b518693797b74eace9e
Hui Qiao <G.HUIQIAO@outlook.com>
2022-06-16 16:38:00 +0800

    完成交易日历的更新和读取功能

--- apt/vendor/tspro/security.py
+++ apt/vendor/tspro/security.py
@@ -19,47 +19,35 @@ class security(base , stock):
             exchange:交易所类型 默认更新上海交易所SSE
                     其他交易所包括：SSE上交所,SZSE深交所,CFFEX 中金所,SHFE 上期所,CZCE 郑商所,DCE 大商所,INE 上能源
         开始和结束日期由stock.XXX来定义
+        更新逻辑：
+            1. is_open在拉去api和数据导入阶段，不进行判断，导入全部的交易日和非交易日
+            2. 数据导入进行差值导入，即比较新数据和原数据库，差异的部分进行导入
         """
         #打印标题
         print("############正在准备更新security证券日历信息###########")
+        #1. 读取api中的交易日数据
         df_cal = self.pro.trade_cal(exchange = exchange, start_date = self.start_date.strftime("%Y%m%d"), end_date = self.end_date.strftime("%Y%m%d"))
-        #从数据库读取已存在的数据
-        df_db = self.get_calendar(exchange = exchange )
-        #将两者进行比较
-        df_diff = pd.concat([df_cal , df_db , df_db] ).drop_duplicates(keep = False)
-        print(df_diff)
-        #将code的索引转换成列
-        df_security.reset_index(level = 0 , inplace = True)
-        #重命名为code
-        df_security.rename(columns = {'index':'code'} , inplace = True)
-        #print(df_security)
-        #dataframe列名的数据类型进行映射
-        dtypedict = {
-            'code': NVARCHAR(length = 24),
-            'display_name': NVARCHAR(length = 128),
-            'name': NVARCHAR(length = 128),
-            'start_date': Date(),
-            'end_date': Date(),
-            'type': NVARCHAR(length = 8)
-                    }
-        #数据保存至数据库
-        if df_security.empty == True:
-            print("原始数据为空，无法导入数据库")
+        #数据类型转换(否则差值计算会出错)       
+        df_cal.rename(columns={"cal_date": "date"} , errors="raise" , inplace = True)
+        df_cal['date'] = pd.to_datetime(df_cal['date'])
+        df_cal['pretrade_date'] = pd.to_datetime(df_cal['pretrade_date'])
+        #df_cal['date'].astype('datetime64[D]')
+        #2. 读取本地数据库中已存在的数据
+        df_db = self.get_calendar(exchange = exchange ,is_open = 2 )
+        #数据类型转换(否则差值计算会出错)
+        df_db['date'] = pd.to_datetime(df_db['date'])
+        #3. 将两者进行比较
+        df_diff = pd.concat([df_cal , df_db , df_db] ).drop_duplicates(subset=['date'],keep = False)
+        #4. 数据保存至数据库
+        if df_diff.empty == True:
+            print("日期差值为空，无需导入数据库")
         else:
-            df_security.to_sql(
-                    name = 'jqdata_security',
+            df_diff.to_sql(
+                    name = 'tspro_calendar',
                     con = self.engine,
                     index = False,
-                    if_exists = 'replace',
-                    index_label='code' , #设置主键(设置未成功)
-                    dtype=dtypedict) #映射列的数据类型
-
-            with self.engine.connect() as con:
-                #设置主键
-                con.execute('ALTER TABLE `jqdata_security` ADD PRIMARY KEY (`code`);')
-                #设置索引（其实主键和索引一致的话，是可以不需要设置索引的）
-                #con.execute('CREATE INDEX index `jqdata_security` (`code`);')
-            print("数据已上传完成(security)")
+                    if_exists = 'append')
+            print(f"数据已上传完成(交易日历)，新增数据{df_diff.shape[0]}条")
 
     def get_security(self , code = None , day = datetime.now()):
         """
@@ -132,18 +120,29 @@ class security(base , stock):
                         if_exists = 'append')
                 print("%s 数据已上传完成(money flow)" % (code))
 
-    def get_calendar(self , exchange = 'SSE' , is_open = 1 ):
+    def get_calendar(self , exchange = 'SSE' , is_open = 1 , all = False):
         """
         获取交易日历
         输入：
             exchange：交易所代码 默认全部使用上交所信息 SSE上交所 SZSE深交所
             is_open：获取交易日的类型 默认剔除非交易日
+                    0 非交易日
+                    1 交易日
+                    2 所有数据（用于交易日更新时进行两个df比较，非正常参数，平时不用）
         输出：
             date 交易日期
             is_open 交易日的类型
         """
-        query = f"select date,is_open FROM tspro_calendar WHERE exchange = '{exchange}' and is_open = {is_open} and date BETWEEN '{self.start_date.date()}' and '{self.end_date.date()}'" 
+        if is_open == 2:
+            #读取全部数据
+            query = f"select exchange,date,is_open,pretrade_date FROM tspro_calendar WHERE exchange = '{exchange}' and date BETWEEN '{self.start_date.date()}' and '{self.end_date.date()}'" 
+        else:
+            #按正常逻辑对is_open进行处理
+            query = f"select date,is_open FROM tspro_calendar WHERE exchange = '{exchange}' and is_open = {is_open} and date BETWEEN '{self.start_date.date()}' and '{self.end_date.date()}'" 
         df_db = pd.read_sql_query(query , self.engine)
+        #排序和数据归一化
+        df_db['date'] = pd.to_datetime(df_db['date'])
+        df_db.sort_values(by = ['date'] , ascending = True , inplace = True)
         if df_db.empty == True:
             #数据库不存在数据
             return pd.DataFrame()
@@ -154,8 +153,8 @@ class security(base , stock):
 if __name__=="__main__":
     #测试交易日历功能
     cal = security()
-    cal.start_date = datetime(2022,1,1)
-    cal.end_date = datetime(2022,9,1)
+    cal.start_date = datetime(1991,1,1)
+    cal.end_date = datetime(1991,9,1)
     cal.update_calendar()
 
     a = base()
