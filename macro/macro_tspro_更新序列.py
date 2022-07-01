3d86625b66db3fb77fcded6f9c254d9c8b7dabaa
Hui Qiao <G.HUIQIAO@outlook.com>
2022-07-01 16:34:03 +0800

    增加更新序列的脚本，这是今后TSPRO数据更新的入口

--- macro/macro_tspro_更新序列.py
+++ macro/macro_tspro_更新序列.py
@@ -8,17 +8,24 @@ from apt.vendor.tspro.security import security  as security
 from apt.vendor.tspro.data import data as data
 
 #1. 初始化
-a = data()
+a = data(myauth = True)
 a.code ='600038.sh'
-a.start_date= datetime(2021,1,1)
-a.end_date = datetime(2022,6,1)
-
+a.start_date= datetime(2022,1,1)
+a.end_date = datetime(2020,2,1)
 #2. 更新证券代码库
+#sec = security()
+#security.update_security(a)
+
+#更新交易日历
+
 
 #3. 更新股票日线数据
+a.update_day()
 
 #4. 更新股票小时线数据
-
+a.ktype = '1m'
+a.update_sequence_add(type = '1m')
+#a.update_sequence_launch()
 #5. 更新ETF日线数据
 
 #6. 更新ETF小时线数据
