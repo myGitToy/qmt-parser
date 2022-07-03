46038937cede07918ffc183364b53f67ff42005f
Hui Qiao <G.HUIQIAO@outlook.com>
2022-07-03 15:15:25 +0800

    tspro和jqdata数据测试完成

--- demo/demo数据比较模块.py
+++ demo/demo数据比较模块.py
@@ -2,22 +2,24 @@ import pandas as pd
 from apt.qsp_universal.base import base
 from datetime import datetime
 # 显示所有列
-pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 #测试ts复权因子
 tspro = base()
-tspro.code ='603986.sh'
+tspro.code ='601318.sh'
 tspro.vendor = tspro.vendor.tusharePro
-tspro.start_date= datetime(2022,7,1)
-tspro.end_date = datetime(2022,7,20)
-tspro.fq = tspro.复权.不复权
+tspro.start_date= datetime(1990,5,6)
+tspro.end_date = datetime(2022,7,10)
+tspro.fq = tspro.复权.动态复权
+tspro.ktype = '1m'
 #ts数据
 df_ts = tspro.get_k_data()
+#[['code','date','close','factor']]
 print(df_ts)
 #jqdata数据
 tspro.code = '603986.XSHG'
 tspro.vendor = tspro.vendor.jqdata
 df_jqdata = tspro.get_k_data()
-print(df_jqdata)
+#print(df_jqdata)
 
     
