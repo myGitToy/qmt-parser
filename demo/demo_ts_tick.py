9839a74dd3837f0741350902e42de9d02c18b646
Hui Qiao <G.HUIQIAO@outlook.com>
2022-07-08 22:35:03 +0800

    删除大量历史测试模块

--- demo/demo_ts_tick.py
+++
@@ -1,8 +0,0 @@
-from apt.os.data_tick import Data_tick
-import datetime
-
-a = Data_tick()
-start = datetime.datetime(2021,6,20)
-end = datetime.datetime(2021,6,29)
-a.update_v2(start_date = start,end_date = end)
-#df = a.get_tick_data(code = '000001' , day='2020-01-07')
--- demo/demo数据比较模块.py
+++ demo/demo数据比较模块.py
@@ -1,21 +1,29 @@
 import pandas as pd
 from apt.qsp_universal.base import base
 from datetime import datetime
+from mpl_finance import candlestick_ochl
+import matplotlib.pyplot as plt
 # 显示所有列
 #pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-#测试ts复权因子
+#pd.set_option('display.max_rows', None)
+
 tspro = base()
 tspro.code ='601318.sh'
 tspro.vendor = tspro.vendor.tusharePro
-tspro.start_date= datetime(1990,5,6)
-tspro.end_date = datetime(2022,7,10)
+tspro.start_date= datetime(2022,7,5,8)
+tspro.end_date = datetime(2022,7,7,16)
 tspro.fq = tspro.复权.动态复权
-tspro.ktype = '1m'
+tspro.ktype = '1d'
+
 #ts数据
-df_ts = tspro.get_all_code()
+df_ts = tspro.get_k_data()
+df_ts['close'].plot()
+# 先画日K线
+#fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(20, 8), dpi=80)
+#candlestick_ochl(axes, df_ts.values, width=0.2, colorup='r', colordown='g')
+#plt.show()
 #[['code','date','close','factor']]
-print(df_ts)
+#print(df_ts)
 #jqdata数据
 tspro.code = '603986.XSHG'
 tspro.vendor = tspro.vendor.jqdata
