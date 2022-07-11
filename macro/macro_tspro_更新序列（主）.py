72116aee1c36eaa6d82070065dce4f06476ca596
Hui Qiao <G.HUIQIAO@outlook.com>
2022-07-11 11:31:34 +0800

    对每日更新流程进行了修订，加入了ETF类数据的支持

--- macro/macro_tspro_更新序列（主）.py
+++ macro/macro_tspro_更新序列（主）.py
@@ -8,25 +8,30 @@ from apt.vendor.tspro.data import data as data
 #1. 初始化
 a = data(myauth = True)
 a.code ='600038.sh'
-a.start_date= datetime(2021,1,1)
-a.end_date = datetime(2022,7,9)
-#2. 更新证券代码库
+a.start_date= datetime(2022,7,8)
+a.end_date = datetime.now()
+#2. 更新证券代码库(stock和ETF资产)
 sec = security()
 sec.update_security()
+sec.update_security_ETF()
 
 #更新交易日历
 sec.update_calendar()
 
-#3. 更新股票日线数据和复权因子(2021/1/1)    最后更新日期2022/7/1含
+#3. 更新股票日线数据和复权因子（忽略）
 a.ktype = '1d'
-#a.update_day()
-a.update_factor()
+a.update_day(flag_verify_db = False)          #最后更新日期2022/7/8含
+a.update_factor(flag_verify_db = False)
 
-#4. 更新股票小时线数据   60分钟线最后更新日期2022/7/1含；5分钟线2020全年写入更新序列
+#5. 更新ETF日线数据和复权因子
+a.update_day_ETF()          #包含2020年起的数据  最后更新日期2022/7/8含
+a.update_factor_ETF() 
+
+#4. 更新股票小时线数据   60分钟线最后更新日期2022/7/6含；5分钟线2020全年写入更新序列
 #目前更新序列剩余18700，预计耗时13小时（每分钟预计可更新24-27组，每组约5800条左右）
-a.ktype = '1m'
-#a.update_sequence_add(type = '1d')
-a.update_sequence_launch()
-#5. 更新ETF日线数据
+#每个月数据量约1.5GB（纯数据库 不含索引） 含索引约2.2GB/月
+a.ktype = '60m'
+a.update_sequence_add(myclass = 'stock' , type = '60m') #更新stock 60分钟线 最后更新日期2022/7/8含
 
-#6. 更新ETF小时线数据
+#6. 更新ETF小时线数据  包含2020/4/20起的数据
+a.update_sequence_add(myclass = 'etf' , type = '60m') #更新etf 60分钟线 最后更新日期 2022/7/8
--- macro/macro_tspro_更新序列（仅更新序列）.py
+++ macro/macro_tspro_更新序列（仅更新序列）.py
@@ -4,8 +4,12 @@ import tushare as ts
 from datetime import datetime,timedelta
 from apt.vendor.tspro.security import security  as security
 from apt.vendor.tspro.data import data as data
-
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
 #1. 更新序列
 a = data()
+a.start_date= datetime(2021,6,1)
+a.end_date = datetime.now()
+#a.update_etf_day()
 a.update_sequence_launch()
 
