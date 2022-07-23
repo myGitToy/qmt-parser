88036c3a6d5d12c7ae9f123fdef5b7be93f27ddd
Hui Qiao <G.HUIQIAO@outlook.com>
2022-07-23 11:37:38 +0800

    资金流向作图 1m 30m代码定型

--- macro/macro_资金流向1m.py
+++ macro/macro_资金流向1m.py
@@ -3,6 +3,7 @@ import numpy as np
 import tushare as ts
 import pandas as pd
 import matplotlib.pyplot as plt
+import mplfinance as mpf  # 替换 import matplotlib.finance as mpf
 from apt.qsp_universal.base import base as data
 # 列名与数据对其显示
 pd.set_option('display.unicode.ambiguous_as_wide', True)
@@ -10,9 +11,9 @@ pd.set_option('display.unicode.east_asian_width', True)
 # 显示所有列
 #pd.set_option('display.max_columns', None)
 a = data()
-a.code = '300033.sz'
-a.start_date = datetime(2022,6,1,4)
-a.end_date = datetime(2022,7,8,16)
+a.code = '002466.sz'
+a.start_date = datetime(2022,6,30,4)
+a.end_date = datetime(2022,7,22,16)
 a.fq = data.复权.动态复权
 a.ktype = '1m'
 a.vendor = a.vendor.tusharePro
@@ -29,7 +30,7 @@ df['money_flow'] = np.where(df['close_diff'] < 0 , -df['money'] , df['money_flow
 df['money_flow'] = np.where(df['close_diff'] == 0 , 0 , df['money_flow'] )
 df['cumsum'] = df['money_flow'].cumsum()
 # 对时间进行一下处理
-df['date'] = pd.to_datetime(df['date'])
+df['date'] = pd.to_datetime(df['date'],format = "%m-%d")
 df.set_index('date',inplace=True)
 #print(df[['date','close','close_diff','money','money_flow','cumsum']])
 #统一各股资金流向的坐标轴，统一按照1个million来计算，也就是对应的1e7
@@ -40,7 +41,12 @@ fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()    # mirror the ax1
 # 使用xticks
 # 这句是核心
-plt.xticks(range(0,len(df.index),90),list(df.index)[::90], rotation = 45)
+plt.xticks(range(0,len(df.index),500),list(df.index)[::500], rotation = 90)
+
+# 创建子图
+#graph_KAV = fig.add_subplot(1, 1, 1)
+#mpf.candlestick2_ochl(graph_KAV, df.open, df.close, df.high, df.low, width=0.5,colorup='r', colordown='g')  # 绘制K线走势
+
 #把X轴的时间进行转换，用来解决时间点断续的问题
 ax1.plot(range(len(df.index)), df['close'], 'g-')
 ax2.plot(range(len(df.index)), df['cumsum'], 'b-')
