5416503430ad48063b11328a19fa59a69c5e500c
Hui Qiao <G.HUIQIAO@outlook.com>
2023-06-16 14:05:34 +0800

    akshare 数据获取模块演示代码

--- demo/demo_akshare_获取K线数据.py
+++ demo/demo_akshare_获取K线数据.py
@@ -7,18 +7,18 @@ pd.set_option('display.max_rows', None)
 
 #测试akshare数据获取模块
 a = data()
-a.start_date= datetime(2023,5,20,8)
-a.end_date = datetime(2023,6,7,16)
-a.ktype = '60m'
+a.start_date= datetime(2023,6,15,8)
+a.end_date = datetime(2023,6,15,16)
+a.ktype = '1m'
 a.code = '601128.SH'
 df = a.get_k_data()
 print(df)
 
 #测试通用数据获取模块
 qsp = qsp_base()
-qsp.start_date= datetime(2023,5,20,8)
-qsp.end_date = datetime(2023,6,7,16)
-qsp.ktype = '60m'
+qsp.start_date= datetime(2023,6,15,8)
+qsp.end_date = datetime(2023,6,15,16)
+qsp.ktype = '1m'
 qsp.vendor = qsp_base.vendor.akshare
 qsp.code = '601128.SH'
 df_qsp = qsp.get_k_data()
