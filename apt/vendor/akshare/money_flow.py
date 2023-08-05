46ba3e044d178b1b82d2b3facb21106db7c2edb0
Hui Qiao <G.HUIQIAO@outlook.com>
2023-08-05 22:44:55 +0800

    调整akshare的资金流向模块，增加1分钟线的资金流向统计，其他函数修改为调用tspro的同名称函数

--- apt/vendor/akshare/money_flow.py
+++ apt/vendor/akshare/money_flow.py
@@ -3,89 +3,101 @@ import pandas as pd
 import tushare as ts
 import akshare as ak
 import sqlalchemy
+from scipy import stats
 from datetime import datetime
-from apt.vendor.akshare.base import base as base
-from apt.vendor.akshare.base import stock as stock
-from apt.vendor.akshare.data import data as data2
+#from apt.vendor.akshare.base import base as base
+#from apt.vendor.akshare.base import stock as stock
+from apt.vendor.akshare.data import data as akdata
+from apt.vendor.tspro.money_flow import money_flow as ts_flow
 from sqlalchemy.types import NVARCHAR , Float, Integer , Date
+from apt.vendor.tspro.security import security as sec
 
-class money_flow(base , stock):
+class money_flow(akdata):
     """
     专门处理资金流向的类
     两种不同的资金流向：V1. 从tspro获取的传统资金流向（联网通过API方式）
                         V2. 从akshare 1分钟线价格 成交量转换过来的资金流向（从数据库获取）
     """
-    def __init__(self):
-        super(base , self).__init__()
-        super(stock , self).__init__()
-    def get_money_flow_V2(self , code = '600001.sh' , day = datetime(2023,1,1)):
+    def daily_update(self , sleep = 0.2):
+        """
+        每日资金流向更新（按日更新）
+        此处实际调用vendor.tspro下面的方法
+        """
+        #获取更新列表（按交易日）
+        return ts_flow.daily_update(self , sleep = sleep)
+
+    def get_money_flow(self , rolling_list = [3,5,10,20,30,60,120] , to_excel = False):
+        """
+        获取资金流向(tspro资金流向) 
+        此处实际调用vendor.tspro下面的方法
+        """
+        return ts_flow.get_money_flow(self , rolling_list = rolling_list , to_excel = to_excel)
+
+    def get_money_flow_1min(self , rolling_list = [3,5,10,20,30,60,120] , to_excel = False):
         """
         从akshare 1分钟线价格 成交量转换过来的资金流向
-        【输入】：
-            code 证券代码
-            day 交易日期
+        【输入】
+            self.code 证券代码
+            day 日期
+        【返回】
+            单个代码在指定日期区间的资金流向（万元）
         """
-        #交易日检查（暂未开通）
-        super(base,self).
-        self.
-        a = data2()
-        a.
-        df.
-        df2 = stock()
-        df2.
-        data = stock()
-        data.code = code 
-        self.et
-        self.code = code
-        self.end_date = day
-        a = base()
-        df = self.get_k_data().sort_values(by = ['date'] )
-        print(df)
-        #删除9:30和15:00的数据
-        df = df.query('date.dt.time != datetime.strptime("09:30","%H:%M").time()')
-        df = df.query('date.dt.time != datetime.strptime("15:00","%H:%M").time()')
-        df['close_diff'] = df['close'] - df['close'].shift(1)
-        #np.where单独使用，符合条件的返回array组，再使用iloc进行定位和修订
-        df['money_flow'] = np.nan
-        #df.iloc[np.where(df['close_diff'] > 0)]['money_flow'] = 1
-        df['money_flow'] = np.where(df['close_diff'] > 0 , df['money'] , df['money_flow'] )
-        df['money_flow'] = np.where(df['close_diff'] < 0 , -df['money'] , df['money_flow'] )
-        df['money_flow'] = np.where(df['close_diff'] == 0 , 0 , df['money_flow'] )
-        df['cumsum'] = df['money_flow'].cumsum()
-        print(df)
-        # 对时间进行一下处理
-        df['date'] = pd.to_datetime(df['date'],format = "%m-%d")
-        df.set_index('date',inplace=True)
-        #print(df[['date','close','close_diff','money','money_flow','cumsum']])
-        #统一各股资金流向的坐标轴，统一按照1个million来计算，也就是对应的1e7
-        df['cumsum'] = df['cumsum'] / 1000000
-        plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文
-        plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
-        fig, ax1 = plt.subplots()
-        ax2 = ax1.twinx()    # mirror the ax1
-        # 使用xticks
-        # 这句是核心
-        plt.xticks(range(0,len(df.index),100),list(df.index)[::100], rotation = 90)
+        #交易代码校验（跳过）
+        #原因：1分钟线数据支持stock类和etf类
+        #获取数据（强制使用1分钟线）
+        self.ktype = '1m'
+        df_db = self.get_k_data().sort_values(by = ['date'] )
+        #初始化最后需要输出的DataFrame
+        df_flow = pd.DataFrame(columns = [['money_flow']], index = df_db['date'].dt.date.unique())
+        #distinct_dates['date'] = df_db['date'].dt.date.unique()
+        #distinct_dates['date'] = pd.to_datetime(df_db['date'].dt.date.unique()) #时间日期ns64化
+        series_days = df_db['date'].dt.date.unique()
+        #print(distinct_dates.shape[0])
+        for day in series_days:
+            #print(day)
+            df = df_db.query('date.dt.date == @day')
+            #删除9:30和15:00的数据
+            df = df.query('date.dt.time != datetime.strptime("09:30","%H:%M").time()')
+            df = df.query('date.dt.time != datetime.strptime("15:00","%H:%M").time()')
+            df['close_diff'] = df['close'] - df['close'].shift(1)
+            #np.where单独使用，符合条件的返回array组，再使用iloc进行定位和修订
+            df['money_flow'] = np.nan
+            #df.iloc[np.where(df['close_diff'] > 0)]['money_flow'] = 1
+            df['money_flow'] = np.where(df['close_diff'] > 0 , df['money'] , df['money_flow'] )
+            df['money_flow'] = np.where(df['close_diff'] < 0 , -df['money'] , df['money_flow'] )
+            df['money_flow'] = np.where(df['close_diff'] == 0 , 0 , df['money_flow'] )
+            df['cumsum'] = df['money_flow'].cumsum()
+            df_flow.loc[day,'money_flow'] = df.iloc[-1]['cumsum'] /10000
+            #print(distinct_dates)
+        #对输出的df进行调整，列 code|date|money_flow
+        df_flow['code'] = self.code
+        df_flow = df_flow.rename_axis('date').reset_index()
+        #df_flow['money_flow'] = df_flow['money_flow'].apply(lambda x: format(x, '.2f'))
+        #df_flow['date'] = pd.to_datetime(df_flow['date'])
+        #print(df_flow)
+        #基础资金流向列表的计算
+        for n in rolling_list:
+            #滚动窗口累计计算
+            df_flow[f'money_flow_r{n}'] = df_flow['money_flow'].rolling(n).sum()
+            #分位数计算
+            df_flow[f'money_flow_p{n}'] =  df_flow[f'money_flow_r{n}'].rolling(n).apply(lambda x: stats.percentileofscore(x, x.iloc[-1]))
+        if to_excel ==True:
+            #输出EXCEL
+            df_flow.to_excel(f'.\\data\\测试数据\\资金流向_1min_{self.code}.xlsx', sheet_name = f'sheet1' ,  header = True, index = True)
+        #print(df_flow)
+        return df_flow
 
-        # 创建子图
-        #graph_KAV = fig.add_subplot(1, 1, 1)
-        mpf.candlestick2_ochl(ax1, df.open, df.close, df.high, df.low, width=0.5,colorup='r', colordown='g')  # 绘制K线走势
-        plt.gcf().autofmt_xdate()  # 自动旋转日期标记
 
-        #把X轴的时间进行转换，用来解决时间点断续的问题
-        #ax1.plot(range(len(df.index)), df['close'], 'g-')
-        ax2.plot(range(len(df.index)), df['cumsum'], 'b-')
-        ax1.set_xlabel('时间')
-        ax1.set_ylabel('价格', color='g')
-        ax2.set_ylabel('资金流向（百万元人民币）', color='b')
-        plt.title(f'{a.code}[{name}]:{a.start_date.date()}-{a.end_date.date()}资金流向表' )
-        plt.show()
-       
-        pass
                
 if __name__=="__main__":
     #测试资金流向
-    a = money_flow()
-    dd = a.get_1min_flow(code = '600389.sh' , day = datetime(2023,7,18))
+    money = money_flow()
+    money.code = '688349.sh'
+    money.start_date = datetime(2021,1,1)
+    money.end_date = datetime(2023,8,5)
+    money.daily_update()
+    money.ktype = '1d'
+    df = money.get_money_flow_1min(to_excel = True ,rolling_list = [10,20])
+    print(df)
 
     
