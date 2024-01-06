82cedc9dd3afd6d2c2a62e40955beef5572bdef2
Hui Qiao <G.HUIQIAO@outlook.com>
2024-01-06 23:29:03 +0800

    象棋程序更新

--- demo/demo_国际象棋_test.py
+++ demo/demo_国际象棋_test.py
@@ -1,15 +1,26 @@
 import chess
-def save_board(board, filename):
-    with open(filename, 'a') as file:
-        file.write(board.fen() + '\n')
 
-def print_board(board):
-    print(board)
-
-def get_move():
+def get_user_move():
     uci_move = input("Enter your move: ")
-    return 
-    
+    return chess.Move.from_uci(uci_move)
+
+def get_ai_move_V1(board):
+    #最简单的AI算法，只返回所有合法算法中的第一个
+    return list(board.legal_moves)[0]
+
+def get_ai_move_V2(board):
+    best_move = None
+    best_value = float('-inf')
+
+    for move in board.legal_moves:
+        board.push(move)
+        move_value = minimax(board, 2)  # 你可以调整深度来改变AI的强度
+        board.pop()
+        if move_value > best_value:
+            best_value = move_value
+            best_move = move
+
+    return best_move
 def minimax(board, depth):
     if board.is_checkmate():
         return float('-inf') if board.turn else float('inf')
@@ -32,20 +43,13 @@ def minimax(board, depth):
             board.pop()
             min_eval = min(min_eval, eval)
         return min_eval
-
-def get_ai_move(board):
-    best_move = None
-    best_value = float('-inf')
-
-    for move in board.legal_moves:
-        board.push(move)
-        move_value = minimax(board, 2)  # 你可以调整深度来改变AI的强度
-        board.pop()
-        if move_value > best_value:
-            best_value = move_value
-            best_move = move
-
-    return best_move
+    
+def print_board(board):
+    print(board)
+    if board.turn:
+        print("White's turn")
+    else:
+        print("Black's turn")
 
 def main():
     board = chess.Board()
@@ -53,20 +57,17 @@ def main():
     while not board.is_checkmate() and not board.is_stalemate():
         print_board(board)
         if board.turn:  # 如果是白方（用户）的回合
-            #打印可用步骤列表
-            print(list(board.legal_moves))
-            move = get_move()
+            move = get_user_move()
         else:  # 如果是黑方（AI）的回合
-            move = get_ai_move(board)
+            move = get_ai_move_V2(board)
         if move in board.legal_moves:
             board.push(move)
-            #save_board(board, 'chess_game.txt')
         else:
             print("Illegal move. Try again.")
 
     print_board(board)
     if board.is_checkmate():
-        print("Checkmate!") 
+        print("Checkmate!")
     else:
         print("Stalemate.")
 
