bcff65ea8e79be1b5f72956160c8570987008b93
Hui Qiao <G.HUIQIAO@outlook.com>
2024-01-08 22:23:02 +0800

    调参

--- macro/macro_akshare_计算全换手间隔.py
+++ macro/macro_akshare_计算全换手间隔.py
@@ -1,11 +1,12 @@
 from apt.vendor.tspro.data import data as data
 from datetime import datetime
 tspro = data()
-tspro.start_date= datetime(2023,12,1,8)
-tspro.end_date = datetime(2023,12,26,16)
+tspro.start_date= datetime(2020,1,1,8)
+tspro.end_date = datetime(2021,12,31,16)
 #tspro.update_day()
 #tspro.update_day_ETF()
 #添加数据
-tspro.update_cumulative_turnover()
-#更新数据
+#tspro.update_cumulative_turnover()
+#更新数据 2022-2023数据全
+#2020-2021正在分析，已导入
 tspro.analyse_cumulative_turnover()
