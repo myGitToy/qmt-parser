0a4211ad2a08669b46aaabc91b2954b4fa94151d
Hui Qiao <G.HUIQIAO@outlook.com>
2024-08-07 16:24:15 +0800

    同花顺概念模块初步完成搭建

--- apt/vendor/akshare/board.py
+++ apt/vendor/akshare/board.py
@@ -126,34 +126,8 @@ class ths_old(board):
         第一层 更新概念时间表 接口: stock_board_concept_name_ths
         """
         return ak.stock_board_industry_summary_ths()
+    
 if __name__ == "__main__":
     t = ths()
     t.launch_gradio_interface()
-    t.update_concept(file_path = 'C:\\Users\\george\\Downloads\\ths_concept.csv')
-    print(df)
-    #csv数据存盘
-    df.to_csv('/data/stock_board_concept_detail_hist_ths.csv', encoding='utf-8-sig')
-    #导入概念指数
-    df_concept = get_concept_relate(date = '20250806')
-    print(df_concept)
-    df_concept.to_csv('ths_concept.csv',index=False)
-    #循环的导入概念指数中的全部成分股
-    df_all = pd.DataFrame()
-    """
-    df_concept数据结构如下： 
-    concept_name为index
-    其余列：concept_symbol concept_code concept_level concept_levelname concept_rank concept_thscode available_date disabled_date
-    """
-    #历遍df_concept，取出所有concept_thscode不为空的数据，然后调用get_concept_stock(concept_thscode_name)
-    for index, row in df_concept.iterrows():
-        if row['concept_thscode'] != None:
-            print(row['concept_thsc ode'])
-            code_list = get_concept_stocks(row['concept_thscode'])
-            # 返回的code_list为列表，如果为空，则不进行处理，如果不为空，则将其转换为DataFrame，然后进行拼接
-            if len(code_list) > 0:
-                df = pd.DataFrame(code_list, columns=["code"])
-                print(df_concept.columns)
-                df["concept_name"] = index
-                df["concept_thscode"] = row["concept_thscode"]
-                print(f'{index} 已处理完成')
-                # df_all = pd.concat([df_all,df])
+
