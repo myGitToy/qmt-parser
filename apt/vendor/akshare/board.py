82f3c8b06b23c1ea4d5550dc7e1fa75dbab97a0b
Hui Qiao <G.HUIQIAO@outlook.com>
2024-08-07 16:22:35 +0800

    增加同花顺概念列表的导入功能update_concept_detail

--- apt/vendor/akshare/board.py
+++ apt/vendor/akshare/board.py
@@ -15,7 +15,8 @@ class ths(board):
     def __init__(self):
         #使用super继承上级
         super(board , self).__init__()
-    def gradio_interface(self):
+
+    def launch_gradio_interface(self):
         """
         gradio接口，用于处理数据更新
         """
@@ -38,12 +39,14 @@ class ths(board):
                     # 输出框
                     code_output = gr.Textbox(label="输出结果")
             #点击事件
-            concept_submit_button.click(fn=self.update_concept , inputs=concept_file_input , outputs=concept_output)
+            concept_submit_button.click(fn=self.update_concept_index , inputs=concept_file_input , outputs=concept_output)
+            code_submit_button.click(fn=self.update_concept_detail , inputs=code_file_input , outputs=code_output)
             #运行
             demo.launch()
-    def update_concept(self , file_obj):
+
+    def update_concept_index(self , file_obj):
         """
-        读取同花顺概念指数的csv文件并导入数据库
+        读取同花顺概念指数的csv文件并导入数据库[stock_board_ths_concept_index]
         file_obj: gradio传入的文件对象
         必须配合gradio使用
         """
@@ -70,6 +73,37 @@ class ths(board):
                     if_exists = 'append')
             return(f"{df_diff.shape[0]}条差异数据已上传")
 
+    def update_concept_detail(self , file_obj):
+        """
+        读取同花顺概念代码的csv文件并导入数据库[stock_board_ths_concept_detail]
+        file_obj: gradio传入的文件对象
+        必须配合gradio使用
+        """
+        # 从 Gradio 文件组件中提取文件路径
+        file_path = file_obj.name
+        #读取csv文件
+        df_csv = pd.read_csv(file_path, usecols = ['code',  'concept_thscode'])
+        #数据校验，必须包含concept_name	concept_symbol两列
+        if 'code' not in df_csv.columns or 'concept_thscode' not in df_csv.columns:
+            return ValueError('非法的数据结构，请检查文件')
+        #数据有效，与数据库中的数据进行去重后导入
+        df_db = pd.read_sql('select code,concept_thscode from stock_board_ths_concept_detail' , con = self.engine)
+        # 确保 concept_symbol 列的数据类型一致
+        df_csv['code'] = df_csv['code'].astype(str)
+        df_csv['concept_thscode'] = df_csv['concept_thscode'].astype(str)
+        df_db['code'] = df_db['code'].astype(str)
+        df_db['concept_thscode'] = df_db['concept_thscode'].astype(str)       
+        df_diff = pd.concat([df_csv,df_db]).drop_duplicates(subset = ['code','concept_thscode'] , keep = False)
+        #print(df_diff)
+        if df_diff.empty == True:
+            return("数据无更新")
+        else:
+            df_diff.to_sql(
+                    name = f'stock_board_ths_concept_detail',
+                    con = self.engine,
+                    index = False,
+                    if_exists = 'append')
+            return(f"{df_diff.shape[0]}条差异数据已上传")
 
 class ths_old(board):
     """
@@ -94,7 +128,7 @@ class ths_old(board):
         return ak.stock_board_industry_summary_ths()
 if __name__ == "__main__":
     t = ths()
-    t.gradio_interface()
+    t.launch_gradio_interface()
     t.update_concept(file_path = 'C:\\Users\\george\\Downloads\\ths_concept.csv')
     print(df)
     #csv数据存盘
