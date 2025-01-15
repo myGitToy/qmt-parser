d257e6c78d682a67a4bf46d39e812235c1b761f0
Hui Qiao <G.HUIQIAO@outlook.com>
2025-01-15 21:54:54 +0800

    数据源修改到localhost

--- apt/vendor/akshare/base.py
+++ apt/vendor/akshare/base.py
@@ -53,9 +53,9 @@ class base():
                 self.pro = ts.pro_api('55297f16c0119146589e059db315ba28a9412e89ec9f91e538e655b2')
                 self.token = '55297f16c0119146589e059db315ba28a9412e89ec9f91e538e655b2'
         elif rds_host == self.数据源.localhost:
-            self.engine = sqlalchemy.create_engine('mysql+pymysql://root:q19840207@192.168.1.186:3306/stock') #186 ubuntu22LTS
+            #self.engine = sqlalchemy.create_engine('mysql+pymysql://root:q19840207@192.168.1.186:3306/stock') #186 ubuntu22LTS
             #self.engine = sqlalchemy.create_engine('mysql+pymysql://stock_user:atp73V4@localhost:3306/stock')   #dell xps15本地数据库
-            #self.engine = sqlalchemy.create_engine('mysql+pymysql://root:sal62688558@localhost:3306/stock')
+            self.engine = sqlalchemy.create_engine('mysql+pymysql://root:sal62688558@localhost:3306/stock')
             #本地数据源支持脱机访问，其他数据源则不支持脱机
             if self.myauth == True:
                 #初始化ts接口
