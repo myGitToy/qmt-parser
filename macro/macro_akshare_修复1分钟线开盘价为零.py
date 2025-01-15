63f571b1add98a168f3727a832d83a4edb4c3b8e
Hui Qiao <G.HUIQIAO@outlook.com>
2025-01-15 12:52:12 +0800

    daily update

--- macro/macro_akshare_修复1分钟线开盘价为零.py
+++ macro/macro_akshare_修复1分钟线开盘价为零.py
@@ -1,6 +1,6 @@
 from apt.vendor.akshare.data import data as data
 from datetime import datetime
 akdata = data()
-akdata.start_date= datetime(2025,1,3,8)
+akdata.start_date= datetime(2025,1,8,8)
 akdata.end_date = datetime.now()
 akdata.fix_1min_error_v2()
