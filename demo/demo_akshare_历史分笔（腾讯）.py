55b63c4a2f68c513a3d40688052c0f5aac8c4101
george <g.huiqiao@gmail.com>
2025-10-02 23:41:26 +0800

    对腾讯提供的历史分笔数据进行测试

---
+++ demo/demo_akshare_历史分笔（腾讯）.py
@@ -0,0 +1,11 @@
+import akshare as ak
+
+stock_zh_a_tick_tx_js_df = ak.stock_zh_a_tick_tx_js(symbol="sz159949")
+print(stock_zh_a_tick_tx_js_df)
+
+"""
+sh601318 有数据
+sz300100 有数据
+sh510300 有数据
+sz159949 有数据
+"""
